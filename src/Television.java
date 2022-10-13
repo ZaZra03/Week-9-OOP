6bd993054e9c3ca12e96cf6e1ff9c65a72bf05ba
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2022-10-13 13:46:22 +0800

    Converted voltage to int

--- src/Television.java
+++ src/Television.java
@@ -1,9 +1,9 @@
 
-public class Television extends Appliance{
+public class Television extends Appliance {
 	private int channel;
 	private int volume;
-	
-	public Television(String model, String voltage, double wattage) {
+
+	public Television(String model, int voltage, double wattage) {
 		super(model, voltage, wattage);
 		this.channel = 1;
 		this.volume = 10;
@@ -12,34 +12,40 @@ public class Television extends Appliance{
 	public int getChannel() {
 		return channel;
 	}
+
 	public void setChannel(int channel) {
 		this.channel = channel;
 	}
+
 	public int getVolume() {
 		return volume;
 	}
+
 	public void setVolume(int volume) {
 		this.volume = volume;
 	}
 
 	public void MoveChannelUp() {
-		if(channel>0) {
+		if (channel > 0) {
 			this.channel += 1;
-			System.out.println("Channel is moved to Channel "+this.channel);
+			System.out.println("Channel is moved to Channel " + this.channel);
 		}
 	}
+
 	public void MoveChannelDown() {
-		if(channel>0) {
+		if (channel > 0) {
 			this.channel -= 1;
-			System.out.println("Channel is moved to Channel "+this.channel);
+			System.out.println("Channel is moved to Channel " + this.channel);
 		}
 	}
+
 	public void IncreaseVolume() {
 		this.volume += 1;
-		System.out.println("Volume is increased by "+this.volume);
+		System.out.println("Volume is increased by " + this.volume);
 	}
+
 	public void DecreaseVolume() {
 		this.volume -= 1;
-		System.out.println("Volume is decreased by "+this.volume);
+		System.out.println("Volume is decreased by " + this.volume);
 	}
 }

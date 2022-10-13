5596bc2f6fc7c718956855a15b968cb4a1b88663
Mark De Vera <81851406+markintoshplus@users.noreply.github.com>
2022-10-13 13:40:01 +0800

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,98 @@
 import java.io.*;
 
 public class Main {
-			
+
 	private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void main(String[] args) throws IOException {
-		
-		// TODO Auto-generated method stub
+		String modelTV;
+		int voltageTV;
+		double wattageTV;
+		int voltageElectricFan;
+		double wattageElectricFan;
+		int typeElectricFan;
+
 		System.out.println("Create your Television.");
-		System.out.print("Type your TV's model: ");
-		String modelTV= in.readLine();
-		System.out.print("Type your Tv's voltage: ");
-		String voltageTV= in.readLine();
-		System.out.print("Type your Tv's wattage: ");
-		double wattageTV= Double.parseDouble(in.readLine());
-		Television objTelevision = new Television(modelTV,voltageTV,wattageTV);
-		
+		System.out.print("Enter your TV's model: ");
+		modelTV = in.readLine();
+
+		while (true) {
+			try {
+				System.out.print("ENter your TV's voltage: ");
+				voltageTV = Integer.parseInt(in.readLine());
+				if (voltageTV <= 0) {
+					System.out.println("\nError. Value cannot be zero or negative.\n");
+				} else {
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("\nError. Please input valid characters.\n");
+			}
+		}
+
+		while (true) {
+			try {
+				System.out.print("Enter your TV's wattage: ");
+				wattageTV = Double.parseDouble(in.readLine());
+				if (wattageTV <= 0) {
+					System.out.println("\nError. Value cannot be zero or negative.\n");
+				} else {
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("\nError. Please input valid characters.\n");
+			}
+		}
+
+		Television objTelevision = new Television(modelTV, voltageTV, wattageTV);
+
 		System.out.println("\nCreate your Electric Fan.");
-		System.out.print("Type your Electric Fan's model: ");
+		System.out.print("Enter your Electric Fan's model: ");
 		String modelElectricFan = in.readLine();
-		System.out.print("Type your Electric Fan's voltage: ");
-		String voltageElectricFan = in.readLine();
-		System.out.print("Type your Electric Fan's wattage: ");
-		double wattageElectricFan = Double.parseDouble(in.readLine());
-		ElectricFan objElectricFan = new ElectricFan(modelElectricFan,voltageElectricFan,wattageElectricFan);
-		
+
+		while (true) {
+			try {
+				System.out.print("Enter your Electric Fan's voltage: ");
+				voltageElectricFan = Integer.parseInt(in.readLine());
+				if (voltageTV <= 0) {
+					System.out.println("\nError. Value cannot be zero or negative.\n");
+				} else {
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("\nError. Please input valid characters.\n");
+			}
+		}
+
+		while (true) {
+			try {
+				System.out.print("Enter your Electric Fan's wattage: ");
+				wattageElectricFan = Double.parseDouble(in.readLine());
+				if (wattageTV <= 0) {
+					System.out.println("\nError. Value cannot be zero or negative.\n");
+				} else {
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("\nError. Please input valid characters.\n");
+			}
+		}
+
+		ElectricFan objElectricFan = new ElectricFan(modelElectricFan, voltageElectricFan, wattageElectricFan);
+
 		System.out.print("\nSelect your Electric Fan's type: ");
 		System.out.println("1) Desk");
 		System.out.println("2) Stand");
 		System.out.println("3) Wall");
 		System.out.println("4) Ceiling");
 		System.out.println("5) Clip");
-		System.out.print("\nEnter your input: ");	
-		int typeElectricFan =  Integer.parseInt(in.readLine());
-		
-	
-		switch(typeElectricFan) {
+		System.out.print("\nEnter your input: ");
+		typeElectricFan = Integer.parseInt(in.readLine());
+
+		switch (typeElectricFan) {
 		case 1:
 			objElectricFan.setFanType(objElectricFan.getFanTypes(0));
 			break;
-			
 		case 2:
 			objElectricFan.setFanType(objElectricFan.getFanTypes(1));
 			break;
@@ -48,12 +102,11 @@ public class Main {
 		case 4:
 			objElectricFan.setFanType(objElectricFan.getFanTypes(3));
 			break;
-		
 		case 5:
 			objElectricFan.setFanType(objElectricFan.getFanTypes(4));
 			break;
 		}
-		
+
 		System.out.println("");
 	}
 }
